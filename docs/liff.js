b768ec14b6b27e5989edb96cd496ffb258809d5e
kento <kentozuka22@gmail.com>
2020-01-24 00:27:44 +0900

    profile pic testing

--- docs/liff.js
+++ docs/liff.js
@@ -1,14 +1,16 @@
 liff.init({ liffId: '1653770270-Jk1N1OPe' })
-  .then(() => {
-    liff.getProfile()
-      .then(profile => {
-        const fullname = profile.displayName;
-        const name = fullname.match(' ') ? fullname.split(' ')[0] : fullname;
-        document.getElementById('username').innerText = name;
-        document.getElementById('user-profile-id').src = profile.pictureUrl;
-      })
-      .catch((err) => { console.log('error', err) })
-  })
-  .catch((err) => {
-    console.log(err.code, err.message)
+    .then(() => {
+        if (accessToken) {
+            liff.getProfile()
+                .then(profile => {
+                    const fullname = profile.displayName
+                    const name = fullname.match(' ') ? fullname.split(' ')[0] : fullname
+                    document.getElementById('username').innerText = name
+                    document.getElementById('user-profile-id').src = profile.pictureUrl
+                })
+                .catch((err) => { console.log('error', err) })
+        }
+    })
+    .catch((err) => {
+        console.log(err.code, err.message)
     })
