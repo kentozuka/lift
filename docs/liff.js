3bcef47557b6b0320715fb3053c1b4b6a4b588f0
kento <kentozuka22@gmail.com>
2020-01-24 00:42:02 +0900

    testing store and liff

--- docs/liff.js
+++ docs/liff.js
@@ -1,16 +1,16 @@
+import { store } from '../src/store/index'
+
 liff.init({ liffId: '1653770270-Jk1N1OPe' })
-    .then(() => {
-        if (accessToken) {
-            liff.getProfile()
-                .then(profile => {
-                    const fullname = profile.displayName
-                    const name = fullname.match(' ') ? fullname.split(' ')[0] : fullname
-                    document.getElementById('username').innerText = name
-                    document.getElementById('user-profile-id').src = profile.pictureUrl
-                })
-                .catch((err) => { console.log('error', err) })
-        }
-    })
-    .catch((err) => {
-        console.log(err.code, err.message)
-    })
+  .then(() => {
+    liff.getProfile()
+      .then(profile => {
+        store.state.liffData = profile
+        const fullname = profile.displayName
+        const name = fullname.match(' ') ? fullname.split(' ')[0] : fullname
+        store.state.username = name
+      })
+      .catch((err) => { console.log('error', err) })
+  })
+  .catch((err) => {
+    console.log(err.code, err.message)
+  })
